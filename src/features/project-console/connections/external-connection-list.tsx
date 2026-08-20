a550f2ac32fd6edf13cef0a37dc23c6c9f4357f4
Ibrahim L <ibrahim.lahouar@kubotal.io>
2026-08-20 03:36:53 +0200

    perf(lists): paginate tables, debounce the filter, batch streamed log lines

--- src/features/project-console/connections/external-connection-list.tsx
+++ src/features/project-console/connections/external-connection-list.tsx
@@ -334,7 +334,7 @@ export function ExternalConnectionList() {
       />
 
       <div className="table-wrapper">
-        <DataTable
+        <DataTable paginator rows={25} paginatorTemplate="PrevPageLink PageLinks NextPageLink" alwaysShowPaginator={false}
           value={connections}
           loading={loading}
           globalFilter={globalFilter}
